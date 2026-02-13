5bc9043a1d4e7dfada2136fc0c0777e5f593ee62
pranjal134-alt <sharmaharishankar700@gmail.com>
2026-02-13 17:39:12 +0530

    new

---
+++ 1.py
@@ -0,0 +1,6 @@
+from difflib import SequenceMatcher
+with open('demo1.txt')  as one_file ,open ('demo2.txt') as two_file:
+    data_file1=one_file.read()
+    data_file2=two_file.read()
+    match=SequenceMatcher(None,data_file1,data_file2).ratio()
+    print(f"the plgiarized content is {match * 100}%")
